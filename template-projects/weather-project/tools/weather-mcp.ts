16c5f3363653a401bdd82b8207099eb1f83b5038
Gaurav Varma <gaurav@inkeep.com>
2025-09-29 12:46:04 -0700

    added image

--- template-projects/weather-project/tools/weather-mcp.ts
+++ template-projects/weather-project/tools/weather-mcp.ts
@@ -4,4 +4,5 @@ export const weatherMcpTool = mcpTool({
   id: 'weather-mcp',
   name: 'Weather',
   serverUrl: 'https://weather-mcp-hazel.vercel.app/mcp',
+  imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-ios-weather-icon-svg-download-png-461610.png?f=webp',
 });
